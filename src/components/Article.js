f45d83b8d333b9ab73f6c67e9d3ec2bdb3a07a1f
Kristine Kochie <kristinekochie@gmail.com>
2022-06-30 13:09:44 -0400

    commit tree b68bee4b372cd74019def6ebfef84b533338a7aa

---
+++ src/components/Article.js
@@ -0,0 +1,24 @@
+
+function emojiList(minutes) {
+    const time = minutes < 30 ? 5 : 10;
+    const emoji = minutes < 30 ? "☕️" : "🍱";
+
+    let emojis = "";
+    for (let i=0; i < minutes; i += time) {
+        emojis += emoji;
+    }
+    return emojis;
+}
+
+function Article({title, date="January 1, 1970", preview, minutes}) {
+    const emojis = emojiList(minutes);
+    return (
+        <article>
+            <h3>{title}</h3>
+            <small>{date} {emojis} {minutes} min read</small>
+            <p>{preview}</p>
+        </article>
+    )
+}
+
+export default Article;
